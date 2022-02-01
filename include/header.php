2b77a67b86e4d2b6fddd690bfb497cd9036ddcbd
palzivar <a.h.shirazi1380.10@gmail.com>
2022-02-02 00:07:30 +0330

    adding header and footer and favicon

---
+++ include/header.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<head>
+    <meta charset="UTF-8">
+    <meta content="IE=ed">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">\
+    <link rel="apple-touch-icon" sizes="180x180" href="fav/favicon_io/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="fav/favicon_io/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="fav/favicon_io/favicon-16x16.png">
+    <link rel="manifest" href="fav/favicon_io/site.webmanifest">
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Black Resume</title>
+    <?php include 'style/style.php'?>
+</head>
