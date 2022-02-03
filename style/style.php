512d398fb6c8871774f81b7350be5bbecf31af75
palzivar <a.h.shirazi1380.10@gmail.com>
2022-02-03 23:18:29 +0330

    building the navbar setting the background

--- style/style.php
+++ style/style.php
@@ -1,3 +1,44 @@
 <style>
+    * {
+        margin: 0;
+        padding: 0;
+        border-spacing: 0;
+    }
 
+    body {
+        font-family: "Courier 10 Pitch", serif;
+    }
+
+    #offcanvas {
+        background: black;
+    }
+
+    #background {
+        background-image: url("image/4141070.jpg");
+        height: 100vh;
+        /*background-attachment: revert;*/
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;
+    }
+
+    #navbar-toggler {
+        padding: 10px;
+        border: none;
+    }
+
+    /*changing the bootstrap css*/
+    .btn:focus {
+        outline: none !important;
+        box-shadow: none !important;
+        border: none;
+    }
+
+    .nav-item {
+        font-size: 20px;
+    }
+
+    .nav-hover:hover {
+        color: #a0a0a0!important;
+    }
 </style>
